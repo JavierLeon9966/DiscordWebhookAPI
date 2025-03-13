5feda8651845eb5e75152dcc66ac0c7d42128b41
Javier León <58715544+JavierLeon9966@users.noreply.github.com>
2025-03-13 01:44:09 -0300

    Specify non-thread-safe classes

--- src/CortexPE/DiscordWebhookAPI/task/DiscordWebhookSendTask.php
+++ src/CortexPE/DiscordWebhookAPI/task/DiscordWebhookSendTask.php
@@ -34,21 +34,22 @@ use CortexPE\DiscordWebhookAPI\Message;
 use CortexPE\DiscordWebhookAPI\Webhook;
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\Server;
+use pocketmine\thread\NonThreadSafeValue;
 
 class DiscordWebhookSendTask extends AsyncTask {
-	/** @var Webhook */
+	/** @var \pocketmine\thread\NonThreadSafeValue<Webhook> */
 	protected $webhook;
-	/** @var Message */
+	/** @var \pocketmine\thread\NonThreadSafeValue<Message> */
 	protected $message;
 
 	public function __construct(Webhook $webhook, Message $message){
-		$this->webhook = $webhook;
-		$this->message = $message;
+		$this->webhook = new NonThreadSafeValue($webhook);
+		$this->message = new NonThreadSafeValue($message);
 	}
 
 	public function onRun():void{
-		$ch = curl_init($this->webhook->getURL());
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($this->message));
+		$ch = curl_init($this->webhook->deserialize()->getURL());
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($this->message->deserialize()));
 		curl_setopt($ch, CURLOPT_POST,true);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
